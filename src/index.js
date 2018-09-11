eee486340e2587cd31bbf42d949f09081ae53ba8
kharuk <frends.nastya@gmail.com>
2018-09-12 00:49:47 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 
 class Sorter {
-  constructor() {
-    this.originalArray = [];
-    this.compare = (a,b) => a - b;
-  }
+    constructor() {
+        this.originalArray = [];
+        this.compare = (a,b) => a - b;
+    }
 
-  add(element) {
-    this.originalArray.push(element);
-  }
+    add(element) {
+        this.originalArray.push(element);
+    }
 
-  at(index) {
-     return this.originalArray[index];
-  }
+    at(index) {
+        return this.originalArray[index];
+    }
 
-  get length() {
-    return this.originalArray.length;
-  }
+    get length() {
+        return this.originalArray.length;
+    }
 
-  toArray() {
-      return this.originalArray;//valueOf
-  }
+    toArray() {
+        return this.originalArray;//valueOf
+    }
 
-  sort(indices) {
-      let arrayForSorting = [];
-      if (indices.length > 1){
-          for (let i = 0; i < indices.length; i++){
-              arrayForSorting.push(this.originalArray[indices[i]]);
-          }
-          arrayForSorting.sort(this.compare);
-          indices.sort((a,b) => a - b);
-          for (let i = 0; i < indices.length; i++){
-              this.originalArray[indices[i]] = arrayForSorting[i];
-          }
-      }
-  }
+    sort(indices) {
+        let arrayForSorting = [];
+        if (indices.length > 1){
+            for (let i = 0; i < indices.length; i++){
+                arrayForSorting.push(this.originalArray[indices[i]]);
+            }
+            arrayForSorting.sort(this.compare);
+            indices.sort((a,b) => a - b);
+            for (let i = 0; i < indices.length; i++){
+                this.originalArray[indices[i]] = arrayForSorting[i];
+            }
+        }
+    }
 
-  setComparator(compareFunction) {
-    return this.compare = compareFunction;
-  }
+    setComparator(compareFunction) {
+        return this.compare = compareFunction;
+    }
 }
 
 module.exports = Sorter;
